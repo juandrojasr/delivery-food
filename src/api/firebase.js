31ee5202c0d9dbc4da815fe619013f7b32028f53
juandrojasr <juandrojasr97@gmail.com>
2023-02-20 22:14:51 -0500

    feat: firebase

---
+++ src/api/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBnxkMZGez-LZ4HQV4DI83MCmHDcnPKpLM",
+  authDomain: "delivery-food-57839.firebaseapp.com",
+  projectId: "delivery-food-57839",
+  storageBucket: "delivery-food-57839.appspot.com",
+  messagingSenderId: "721305915781",
+  appId: "1:721305915781:web:462d48baa084f94d0d34ec"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
